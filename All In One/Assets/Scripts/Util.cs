68ed7bf696ef3648db1853db7b63a3cbe6f1fdba
Stéphan Taljaard <steptaljaard@gmail.com>
2015-05-03 19:48:23 +0200

    Add Util.isObstructed

---
+++ All In One/Assets/Scripts/Util.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class Util
+{
+    /// <summary>
+    /// Checks if the line from origin is obstructed.
+    /// </summary>
+    /// <param name="origin">Origin of the line to be checked.</param>
+    /// <param name="direction">Direction wherein the line goes.</param>
+    /// <param name="length">The length of the line.</param>
+    /// <returns></returns>
+    public static bool isObstructed(Vector3 origin, Vector3 direction, float length)
+    {
+        RaycastHit[] hits;
+        hits = Physics.RaycastAll(origin, direction, length);
+
+        if (hits.Length != 1)
+            return false;
+        else
+            return true;
+    }
+}
